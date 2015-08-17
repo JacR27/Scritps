2022dd02cc83cf41792511774efe9fcbc1e6f859
Joe Rayner <jacryaner58@gmail.com>
2015-08-18 00:14:19 +0100

    I26

--- benchmarking/ibenchmark.sh
+++ benchmarking/ibenchmark.sh
@@ -48,58 +48,10 @@ grep "Elapsed time" ${analysisdir}${processName}.stdout | python ${scriptsDir}ex
 
 python ${scriptsDir}collectlTimeDateConverter.py "$processName" "$analysisdir" "$analysisdir"
 
-export GNUTERM=dumb
-/usr/bin/gnuplot <<\EOF
+cp ${analysisdir}${processName}.tsv ${analysisdir}collectl.tsv
 
-reset
-clear
-workflow = ""
-hostname = ""
-file="collectl.tsv"
-set terminal png size 1500,500
-set output "CPU".hostname.workflow.".png"
+gnuisaac.sh ${analysisdir} 
 
-set xdata time
-set timefmt "%s"
-set format x "%H:%M"
-set xlabel "time"
-set ylabel "Percent CPU"
-#set yrange [0:110]
-set title "CPU usage"." ".hostname." ".workflow
-set style data line
-set key outside
-#set xtic (1000)
-
-plot file using (column("Time")):(column("[CPU]Wait%")):xtic(60) title "[CPU]Wait%", "" using (column("Time")):(column("[CPU]Sys%")) title "[CPU]Sys%", "" using (column("Time")):(column("[CPU]Nice%")) title "[CPU]Nice%"# "" using (column("Time")):(column("[CPU]User%")) title "[CPU]User%"
-
-plot file using (column("Time")):(column("[CPU]User%")) title "[CPU]User%"
-
-replot
-
-set output "memory".hostname.workflow.".png"
-set xdata time
-set timefmt "%s"
-set format x "%H:%M"
-set xlabel "time"
-set ylabel "GB"
-set title "Memory usage"." ".hostname." ".workflow
-set style data line
-
-plot file using (column("Time")):(column("[MEM]Commit")) title "[MEM]Commit", "" using (column("Time")):(column("[MEM]Cached")) title "[MEM]Cashed"
-
-set output "IO".hostname.workflow.".png"
-set xdata time
-set timefmt "%s"
-set format x "%H:%M"
-set xlabel "time"
-set ylabel "MB/s
-set title "Disk IO"." ".hostname." ".workflow
-set style data line
-plot file using (column("Time")):(column("[DSK]ReadKBTot")) title "[DSK]ReadKBTot%", "" using (column("Time")):(column("[DSK]WriteKBTot")) title "[DSK]WriteKBTot%"
-set output
-#
-EOF
-
-rm collectl.tsv
+rm ${analysisdir}collectl.tsv
 
 exit
